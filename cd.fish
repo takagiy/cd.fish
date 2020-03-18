368afebab473a16e9451b746a51440ba15fd6315
takagiy <takagiy.4dev@gmail.com>
2020-03-18 15:14:46 +0900

    invoking recursively if subdir is selected

--- cd.fish
+++ cd.fish
@@ -3,7 +3,7 @@ function com_github_takagiy_cd_fish_cd_impl
   if set -l i_ (contains -i $path_ $com_github_takagiy_cd_fish_history)
     set -e com_github_takagiy_cd_fish_history[$i_]
   end &&
-  if not string match --all $path_ $HOME > /dev/null
+  if [ $path_ != $HOME ]
     set -Ua com_github_takagiy_cd_fish_history $path_
   end &&
   builtin cd $argv
@@ -14,22 +14,47 @@ function com_github_takagiy_cd_fish_clear_history
 end
 
 function cd --description "Change directory"
+  set -l argv_noflags_ (seq 0)
+  set -l include_history_ 1
+  set -l include_home_ 1
+  set -l repeat_ 1
   for arg in $argv
     if [ $arg = "--clear-history" ]
       com_github_takagiy_cd_fish_clear_history
       return
+    else if [ $arg = "--no-history" ]
+      set include_history_ 0
+    else if [ $arg = "--no-home" ]
+      set include_home_ 0
+    else if [ $arg = "--no-repeat" ]
+      set repeat_ 0
+    else
+      set -a argv_noflags_ $arg
     end
   end
   if not set -q com_github_takagiy_cd_fish_history
     com_github_takagiy_cd_fish_clear_history
   end
-  if count $argv > /dev/null
-    com_github_takagiy_cd_fish_cd_impl $argv
+  if count $argv_noflags_ > /dev/null
+    com_github_takagiy_cd_fish_cd_impl $argv_noflags_
   else
-    :
-    set -l history_ $com_github_takagiy_cd_fish_history &&
-    set -l children_ (find -maxdepth 1 -type d | sort) &&
-    set -l dest_ (string join \n $children_ $history_ $HOME .. | fzf --tac --height 50%) &&
-    com_github_takagiy_cd_fish_cd_impl $dest_
+    set -l selections_ (seq 0) &&
+    set -a selections_ (find -maxdepth 1 -type d | sort | tail +2) &&
+    if [ $include_history_ = 1 ]
+      set -a selections_ $com_github_takagiy_cd_fish_history
+    end &&
+    if [ $include_home_ = 1 ]
+      set -a selections_ $HOME
+    end &&
+    set -a selections_ . .. &&
+    set -l dest_ (string join \n $selections_ | fzf --tac --height 50%) &&
+    if [ $dest_ = "." ] || not string match ".*" $dest_
+      com_github_takagiy_cd_fish_cd_impl $dest_
+    else
+      builtin cd $dest_
+    end &&
+    if string match '.*' $dest_ > /dev/null && [ $dest_ != "." ] && [ $repeat_ = 1 ]
+      cd --no-home --no-history
+    end
   end
 end
